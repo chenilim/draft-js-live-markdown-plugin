f584f12f749e3afb5b206383eeea22ae1daadb25
Chen-I Lim <chenilim@gmail.com>
2021-12-07 14:12:45 -0800

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = [
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
+            loader: 'babel-loader',
+			options: { presets: ['@babel/env','@babel/preset-react'] }
           }
         }
       ]
